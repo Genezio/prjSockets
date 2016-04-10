9d747de9f3758018cac6f1922de79b896f079632
Genezio <geneziocosta@hotmail.com>
2016-04-10 19:01:15 -0300

    Continuado servidor

--- prjSockets/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ prjSockets/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace frmServidor
 {
     partial class frm_principal
     {
@@ -30,18 +30,23 @@
         {
             this.tb_Control_server = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.gBox_propreties = new System.Windows.Forms.GroupBox();
-            this.tx_ip = new System.Windows.Forms.TextBox();
-            this.lb_ip = new System.Windows.Forms.Label();
-            this.lb_porta = new System.Windows.Forms.Label();
-            this.tx_porta = new System.Windows.Forms.TextBox();
-            this.bt_iniciar = new System.Windows.Forms.Button();
-            this.bt_desconecta = new System.Windows.Forms.Button();
+            this.lb_envia = new System.Windows.Forms.Label();
+            this.lb_separador = new System.Windows.Forms.Label();
+            this.tx_enviaComando = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rt_monitor = new System.Windows.Forms.RichTextBox();
+            this.bt_desconecta = new System.Windows.Forms.Button();
+            this.bt_iniciar = new System.Windows.Forms.Button();
+            this.tx_porta = new System.Windows.Forms.TextBox();
+            this.lb_porta = new System.Windows.Forms.Label();
+            this.lb_ip = new System.Windows.Forms.Label();
+            this.tx_ip = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tb_Control_server.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.gBox_propreties.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tb_Control_server
@@ -65,18 +70,11 @@
             this.tabPage1.Text = "Propriedades";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(908, 481);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Supervisório";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // gBox_propreties
             // 
+            this.gBox_propreties.Controls.Add(this.lb_envia);
+            this.gBox_propreties.Controls.Add(this.lb_separador);
+            this.gBox_propreties.Controls.Add(this.tx_enviaComando);
             this.gBox_propreties.Controls.Add(this.groupBox1);
             this.gBox_propreties.Controls.Add(this.bt_desconecta);
             this.gBox_propreties.Controls.Add(this.bt_iniciar);
@@ -91,21 +89,78 @@
             this.gBox_propreties.TabStop = false;
             this.gBox_propreties.Text = "Conexão";
             // 
-            // tx_ip
+            // lb_envia
             // 
-            this.tx_ip.Location = new System.Drawing.Point(6, 31);
-            this.tx_ip.Name = "tx_ip";
-            this.tx_ip.Size = new System.Drawing.Size(127, 20);
-            this.tx_ip.TabIndex = 0;
+            this.lb_envia.AutoSize = true;
+            this.lb_envia.Location = new System.Drawing.Point(6, 113);
+            this.lb_envia.Name = "lb_envia";
+            this.lb_envia.Size = new System.Drawing.Size(84, 13);
+            this.lb_envia.TabIndex = 9;
+            this.lb_envia.Text = "Enviar comando";
             // 
-            // lb_ip
+            // lb_separador
             // 
-            this.lb_ip.AutoSize = true;
-            this.lb_ip.Location = new System.Drawing.Point(6, 15);
-            this.lb_ip.Name = "lb_ip";
-            this.lb_ip.Size = new System.Drawing.Size(17, 13);
-            this.lb_ip.TabIndex = 1;
-            this.lb_ip.Text = "IP";
+            this.lb_separador.AutoSize = true;
+            this.lb_separador.Location = new System.Drawing.Point(6, 93);
+            this.lb_separador.Name = "lb_separador";
+            this.lb_separador.Size = new System.Drawing.Size(265, 13);
+            this.lb_separador.TabIndex = 8;
+            this.lb_separador.Text = "---------------------------------------------------------------------------------" +
+    "-----";
+            // 
+            // tx_enviaComando
+            // 
+            this.tx_enviaComando.Location = new System.Drawing.Point(9, 129);
+            this.tx_enviaComando.Name = "tx_enviaComando";
+            this.tx_enviaComando.Size = new System.Drawing.Size(262, 20);
+            this.tx_enviaComando.TabIndex = 7;
+            this.tx_enviaComando.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tx_enviaComando_KeyDown);
+            this.tx_enviaComando.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tx_enviaComando_KeyUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rt_monitor);
+            this.groupBox1.Location = new System.Drawing.Point(9, 155);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 308);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Monitor";
+            // 
+            // rt_monitor
+            // 
+            this.rt_monitor.Location = new System.Drawing.Point(7, 19);
+            this.rt_monitor.Name = "rt_monitor";
+            this.rt_monitor.Size = new System.Drawing.Size(247, 283);
+            this.rt_monitor.TabIndex = 0;
+            this.rt_monitor.Text = "";
+            // 
+            // bt_desconecta
+            // 
+            this.bt_desconecta.Location = new System.Drawing.Point(142, 67);
+            this.bt_desconecta.Name = "bt_desconecta";
+            this.bt_desconecta.Size = new System.Drawing.Size(127, 23);
+            this.bt_desconecta.TabIndex = 5;
+            this.bt_desconecta.Text = "Parar Servidor";
+            this.bt_desconecta.UseVisualStyleBackColor = true;
+            this.bt_desconecta.Click += new System.EventHandler(this.bt_desconecta_Click);
+            // 
+            // bt_iniciar
+            // 
+            this.bt_iniciar.Location = new System.Drawing.Point(9, 67);
+            this.bt_iniciar.Name = "bt_iniciar";
+            this.bt_iniciar.Size = new System.Drawing.Size(124, 23);
+            this.bt_iniciar.TabIndex = 4;
+            this.bt_iniciar.Text = "Iniciar Servidor";
+            this.bt_iniciar.UseVisualStyleBackColor = true;
+            this.bt_iniciar.Click += new System.EventHandler(this.bt_iniciar_Click);
+            // 
+            // tx_porta
+            // 
+            this.tx_porta.Location = new System.Drawing.Point(142, 31);
+            this.tx_porta.Name = "tx_porta";
+            this.tx_porta.Size = new System.Drawing.Size(127, 20);
+            this.tx_porta.TabIndex = 3;
             // 
             // lb_porta
             // 
@@ -117,39 +172,31 @@
             this.lb_porta.Text = "Porta";
             this.lb_porta.Click += new System.EventHandler(this.lb_porta_Click);
             // 
-            // tx_porta
+            // lb_ip
             // 
-            this.tx_porta.Location = new System.Drawing.Point(142, 31);
-            this.tx_porta.Name = "tx_porta";
-            this.tx_porta.Size = new System.Drawing.Size(127, 20);
-            this.tx_porta.TabIndex = 3;
+            this.lb_ip.AutoSize = true;
+            this.lb_ip.Location = new System.Drawing.Point(6, 15);
+            this.lb_ip.Name = "lb_ip";
+            this.lb_ip.Size = new System.Drawing.Size(17, 13);
+            this.lb_ip.TabIndex = 1;
+            this.lb_ip.Text = "IP";
             // 
-            // bt_iniciar
+            // tx_ip
             // 
-            this.bt_iniciar.Location = new System.Drawing.Point(9, 67);
-            this.bt_iniciar.Name = "bt_iniciar";
-            this.bt_iniciar.Size = new System.Drawing.Size(124, 23);
-            this.bt_iniciar.TabIndex = 4;
-            this.bt_iniciar.Text = "Iniciar Servidor";
-            this.bt_iniciar.UseVisualStyleBackColor = true;
+            this.tx_ip.Location = new System.Drawing.Point(6, 31);
+            this.tx_ip.Name = "tx_ip";
+            this.tx_ip.Size = new System.Drawing.Size(127, 20);
+            this.tx_ip.TabIndex = 0;
             // 
-            // bt_desconecta
+            // tabPage2
             // 
-            this.bt_desconecta.Location = new System.Drawing.Point(142, 67);
-            this.bt_desconecta.Name = "bt_desconecta";
-            this.bt_desconecta.Size = new System.Drawing.Size(127, 23);
-            this.bt_desconecta.TabIndex = 5;
-            this.bt_desconecta.Text = "Parar Servidor";
-            this.bt_desconecta.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(9, 112);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(260, 351);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Monitor";
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(908, 481);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Supervisório";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // frm_principal
             // 
@@ -159,10 +206,12 @@
             this.Controls.Add(this.tb_Control_server);
             this.Name = "frm_principal";
             this.Text = "Sockets Servidor";
+            this.Load += new System.EventHandler(this.frm_principal_Load);
             this.tb_Control_server.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.gBox_propreties.ResumeLayout(false);
             this.gBox_propreties.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -180,6 +229,10 @@
         private System.Windows.Forms.Label lb_ip;
         private System.Windows.Forms.TextBox tx_ip;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rt_monitor;
+        private System.Windows.Forms.Label lb_envia;
+        private System.Windows.Forms.Label lb_separador;
+        private System.Windows.Forms.TextBox tx_enviaComando;
     }
 }
 
--- prjSockets/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ prjSockets/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -4,14 +4,22 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace frmServidor
 {
     public partial class frm_principal : Form
     {
+        private int porta;
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+        private NetworkStream newWorkStream;
+        private Thread thInteraction;
+
         public frm_principal()
         {
             InitializeComponent();
@@ -21,5 +29,237 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        //Método responsávael por efetuar a conexão
+        private bool conecta()
+        {
+
+            bool retorno = false;
+
+            try
+            {
+                this.porta = Convert.ToInt32(this.tx_porta.Text);
+                this.tcpListener = new TcpListener(System.Net.IPAddress.Any, this.porta);
+                this.tcpListener.Start();
+                retorno = true;
+
+            }
+            catch
+            {
+
+            }
+            return retorno;
+
+
+        }
+        //Método responsávael por fechar a conexão
+        private void desconecta()
+        {
+
+            if (this.thInteraction != null)
+            {
+                if (this.thInteraction.ThreadState == ThreadState.Running)
+                {
+                    this.thInteraction.Abort();
+                }
+
+            }
+
+            if (this.tcpClient != null)
+            {
+                this.tcpClient.Client.Disconnect(true);
+                this.tcpListener.Stop();
+                setMsg("### Conexões Perdidas ###", true);
+            }
+            
+
+        }
+
+        //Método responsávael por aceitas as conexãoes
+
+        private void acceptConnection()
+        {
+
+            try
+            {
+                this.tcpClient = this.tcpListener.AcceptTcpClient();
+            }
+            catch
+            {
+
+            }
+
+        }
+        //Método responsávael por enviar mensagens
+        private void enviarMensagem(string mensagem)
+        {
+
+            if (podeEscrever())
+            {
+                byte[] sedBytes = Encoding.ASCII.GetBytes(mensagem);//Converte a mensagem para bytes
+                this.newWorkStream.Write(sedBytes, 0, sedBytes.Length);
+
+
+            }
+
+        }
+        //Método responsávael por verificar se pode enviar mensagens
+        private bool podeEscrever()
+        {
+            if (this.newWorkStream == null)
+            {
+                return false;
+            }
+
+            if (this.newWorkStream.CanWrite && this.tcpClient != null)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+
+        }
+
+        //Quando usar therads tem que usar o delegate para não dar erro
+        delegate void delSetMsg(string mensgem, bool isAdm);
+        //Método responsávael por setar mensagens;
+        private void setMsg(string mensagem, bool isAmd)
+        {
+
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new delSetMsg(setMsg), mensagem, isAmd);
+            }
+            else
+            {
+                if (isAmd == true || podeEscrever())
+                {
+                    this.rt_monitor.Text += "\n Server: " + mensagem;
+                }
+            }
+
+
+        }
+
+        //Quando usar therads tem que usar o delegate para não dar erro
+        delegate void delGetMsg(string mensgem);
+        //Método responsávael por pegar as  mensagens do cliente;
+        private void getMsg(string mensagem)
+        {
+
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new delGetMsg(getMsg), mensagem);
+            }
+            else
+            {
+                if (podeEscrever())
+                {
+                    this.rt_monitor.Text += "\n + Client: " + mensagem;
+                }
+            }
+
+        }
+        //Método responsávael por startar o servidor;
+        private void start()
+        {
+
+            if (this.conecta())
+            {
+                this.setMsg("## Aguardando Conexão ...", true);
+
+                this.thInteraction = new Thread(new ThreadStart(monitora));
+                this.thInteraction.IsBackground = true;
+                this.thInteraction.Priority = ThreadPriority.Highest;
+                this.thInteraction.Name = "thInteracao";
+                this.thInteraction.Start();
+
+            }
+        }
+
+        //Método responsávael por ficar monitorando as entradas e saidas;
+        private void monitora()
+        {
+            try
+            {
+                this.acceptConnection();
+                this.setMsg("## Conexão Aceita ...", true);
+
+                do
+                {
+                    this.newWorkStream = this.tcpClient.GetStream();
+                    if (this.newWorkStream.CanRead)
+                    {
+                        byte[] bytes = new byte[this.tcpClient.ReceiveBufferSize];
+                        this.newWorkStream.Read(bytes, 0, Convert.ToInt32(this.tcpClient.ReceiveBufferSize));
+                        //Dado do cliente
+                        string clientData = Encoding.ASCII.GetString(bytes);
+
+                        //Verifica se realmente tem uma mensagem
+
+                        if (clientData.Replace("\0", "").Trim() != "")
+                        {
+                            this.getMsg(clientData);
+
+                        }
+
+
+                    }
+
+                } while (this.tcpClient != null);
+
+                this.desconecta();
+                this.start();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+        private void bt_iniciar_Click(object sender, EventArgs e)
+        {
+            this.start();
+            this.bt_iniciar.Enabled = false;
+        }
+
+        private void bt_desconecta_Click(object sender, EventArgs e)
+        {
+            this.setMsg("## Encerrando conexão com o servidor", true);
+            this.desconecta();
+            this.bt_iniciar.Enabled = true;
+        }
+
+        private void tx_enviaComando_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string mensagem = this.tx_enviaComando.Text;
+                this.enviarMensagem(mensagem);
+                this.setMsg(mensagem, false);
+
+            }
+        }
+
+        private void tx_enviaComando_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.tx_enviaComando.Clear();
+            }
+        }
+
+        private void frm_principal_Load(object sender, EventArgs e)
+        {
+            this.tx_ip.Text = Convert.ToString(System.Net.IPAddress.Any);
+        }
+
+
+
     }
 }
